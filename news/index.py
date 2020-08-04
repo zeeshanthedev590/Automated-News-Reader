bef537500f4d92f8d9a158a984d6f96539e0ce78
zeeshanthedev590 <62704414+zeeshanthedev590@users.noreply.github.com>
2020-08-04 20:16:53 +0500

    Add files via upload

---
+++ news/index.py
@@ -0,0 +1,25 @@
+
+# Akhbaar padhke sunaao
+import requests
+import json
+
+def speak(str):
+    from win32com.client import Dispatch
+    speak = Dispatch("SAPI.SpVoice")
+    speak.Speak(str)
+
+if __name__ == '__main__':
+    speak('Welcome To Zeeshan News!!!')
+    speak("News for today.. Lets begin")
+    url = "https://newsapi.org/v2/top-headlines?sources=the-times-of-india&apiKey=d093053d72bc40248998159804e0e67d"
+    news = requests.get(url).text
+    news_dict = json.loads(news)
+    arts = news_dict['articles']
+    for article in arts:
+        speak(article['title'])
+        print(article['title'])
+        speak("Moving on to the next news..")
+
+    speak("Thanks for listening...")
+
+
